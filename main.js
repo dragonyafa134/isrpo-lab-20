dbbdc9e47c806886cb10aaed3253cfcb60aeeabe
dragonyafa134 <dasha.afanaseva.1206@mail.ru>
2026-03-18 10:56:41 +0300

    Step 7: mini task with DOM events

--- main.js
+++ main.js
@@ -37,3 +37,9 @@ button.addEventListener("click", () => {
         output.style.color = "red";
     }
 });
+
+const clearButton = document.getElementById("clearButton");
+clearButton.addEventListener("click", () => {
+    input.value = "";
+    output.textContent = "";
+});
