0ae7a4a648183d927faaf7a2e4efc4285293fdc9
dragonyafa134 <dasha.afanaseva.1206@mail.ru>
2026-03-18 10:38:56 +0300

    Step 5: click event handling

--- main.js
+++ main.js
@@ -4,3 +4,20 @@ const title = document.getElementById("title");
 const text = document.querySelector(".text");
 console.log(title);
 console.log(text);
+
+
+title.textContent = "DOM ИЗМЕНЕН ЧЕРЕЗ ДС";
+title.style.color = "blue";
+title.style.backgroundColor = "f0f0f0";
+title.style.padding = "10px";
+
+text.style.fontSize = "18px";
+text.style.fontStyle = "italic";
+text.style.color = "darkgreen";
+
+const btn = document.getElementById("btn");
+btn.addEventListener("click", () => {
+    alert("Кнопка нажата");
+    btn.style.backgroundColor = "#4CAF50";
+    btn.style.color = "white";
+});
